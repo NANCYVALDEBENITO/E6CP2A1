adac95479a50356747ba120eda636e85e3fdbf98
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-11-28 19:57:05 -0300

    Ejercicio 5

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,14 @@
-# Crear un método que reciba como parámetro dos números enteros positivos
-# e imprima los números pares que existen entre esos dos números.
+# Crear un metodo que reciba como parametro dos numeros enteros positivos
+# e imprima los numeros pares que existen entre esos dos numeros.
+
+def numeros_pares(numero1,numero2)
+	for n in numero1..numero2
+		if (n % 2).zero?
+			puts n
+		else
+			puts "#{n} no es par"
+		end
+	end
+end
+
+numeros_pares(2,10)

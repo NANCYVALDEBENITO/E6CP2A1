50668b1f89cc7721d1033f9c3b8d6e2eeac9c3b5
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-02 18:21:56 -0300

    Ejericio 1 string 100%

--- 3 strings/ejercicio1.rb
+++ 3 strings/ejercicio1.rb
@@ -1,7 +1,19 @@
-# Dado el siguiente string y caracter, crear un método que reciba como parámetro el string
+# Dado el siguiente string y caracter, crear un metodo que reciba como parametro el string
 # y el caracter. Luego debe buscar si existe ese caracter dentro del string.
-# hint: El método .include? de un string busca si un caracter
-# o string dado está contenido en éste.
+# hint: El metodo .include? de un string busca si un caracter
+# o string dado esta contenido en aste.
 
 cadena = 'Hola Mundo!'
 caracter = 'o'
+
+def exist(string,parameter)
+	if string.include? parameter
+		puts true
+	else 
+		puts false
+	end
+end
+
+exist(cadena,caracter)
+
+
